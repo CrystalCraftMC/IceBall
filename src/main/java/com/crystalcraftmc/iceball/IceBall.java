ed2b7ebde9bec162a95133fa36908416d316a4e0
Justin W. Flory <me@justinwflory.com>
2015-03-24 21:40:22 -0400

    Add plugin metrics

--- src/main/java/com/crystalcraftmc/iceball/IceBall.java
+++ src/main/java/com/crystalcraftmc/iceball/IceBall.java
@@ -22,7 +22,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcstats.Metrics;
 
 import java.io.IOException;
 
@@ -43,7 +42,7 @@ public class IceBall extends JavaPlugin {
 		new IceBallListener(this);
 
         try {
-            Metrics metrics = new Metrics(this);
+            MetricsLite metrics = new MetricsLite(this);
             metrics.start();
         } catch (IOException e) {
             // Failed to submit the stats :-(
