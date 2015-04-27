ea0662d95c3c916c5b65e369765d74f0c7296fa8
Alex <www.woodrow73@gmail.com>
2015-04-27 13:49:00 -0400

    Update GameplayListener.java

--- src/main/java/com/crystalcraftmc/iceball/main/GameplayListener.java
+++ src/main/java/com/crystalcraftmc/iceball/main/GameplayListener.java
@@ -1,22 +1,5 @@
-/*
- * Copyright 2015 CrystalCraftMC
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+package com.crystalcraftmc.snowball.main;
 
-package com.crystalcraftmc.iceball.main;
-
-import com.crystalcraftmc.iceball.api.Utility;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -27,12 +10,14 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import com.crystalcraftmc.snowball.api.Utility;
+
 /**Handles events related to gameplay*/
 public class GameplayListener implements Listener {
 	
-	IceBall plugin;
+	Snowball plugin;
 	
-	public GameplayListener(IceBall plugin) {
+	public GameplayListener(Snowball plugin) {
 		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
@@ -41,7 +26,7 @@ public class GameplayListener implements Listener {
 	public void tpBelow(PlayerInteractEvent e) {
 		if(e.getAction() == Action.PHYSICAL) {
 			Block b = e.getClickedBlock();
-			if(Utility.isInsideSnowball(b.getLocation(), plugin)) {
+			if(Utility.isInsideSnowball(b.getLocation(), plugin, false)) {
 				if(b.getType() == Material.IRON_PLATE) {
 					Player p = e.getPlayer();
 					int centerx = (plugin.snowballArea[0]+plugin.snowballArea[3])/2;
@@ -68,7 +53,7 @@ public class GameplayListener implements Listener {
 	public void noPvp(EntityDamageByEntityEvent e) {
 		if(e.getEntity() instanceof Player) {
 			Player p = (Player)e.getEntity();
-			if(Utility.isInsideSnowball(p.getLocation(), plugin)) {
+			if(Utility.isInsideSnowball(p.getLocation(), plugin, false)) {
 				if(e.getDamage() > 0) {
 					e.setCancelled(true);
 				}
